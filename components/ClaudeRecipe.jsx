c8303961ae937781638f7b790b42aed89dfe8a64
aysenuron <onaranays@gmail.com>
2025-03-28 16:15:48 +0300

    Delete H2 on recipes.

--- components/ClaudeRecipe.jsx
+++ components/ClaudeRecipe.jsx
@@ -3,7 +3,6 @@ import ReactMarkdown from "react-markdown";
 export default function ClaudeRecipe(props) {
     return (
         <section className="suggested-recipe-container" aria-live="polite">
-            <h2>Chef Recommends:</h2>
             <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
     )
